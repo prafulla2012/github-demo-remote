4b97772dddcd912b0511940c4074a8ba548bb746
prafulla_chaudhari <prafulla_chaudhari@persistent.com>
2022-01-21 15:29:40 +0530

    Modified function headers

--- maths.py
+++ maths.py
@@ -1,15 +1,15 @@
-#Add implementation
+#Add implementation.
 def add(x,y)
 	return x+y
 	
-#Subtract implementation	
+#Subtract implementation.	
 def sub(x,y)
 	pass
 	
-#Multiply implementation
+#Multiply implementation.
 def multiply(x,y)
 	pass
 	
-#Divide implementation
+#Divide implementation.
 def divide(x,y)
 	pass
